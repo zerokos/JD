736f174b593132fdc2aeeab036869dade2a9c4bd
zerokos <zerokos@qq.com>
2020-12-12 07:57:13 +0800

    8

--- .github/jd_sharecodes.sh
+++ .github/jd_sharecodes.sh
@@ -6,6 +6,7 @@ PETSHARECODES_ARR=($PETSHARECODES)
 PLANT_BEAN_SHARECODES_ARR=($PLANT_BEAN_SHARECODES)
 JXFACTORY_SHARECODES_ARR=($JXFACTORY_SHARECODES)
 DDFACTORY_SHARECODES_ARR=($DDFACTORY_SHARECODES)
+DREAM_FACTORY_SHARE_CODES_ARR=($DREAM_FACTORY_SHARE_CODES)
 
 for sharecode in ${FRUITSHARECODES_ARR[@]}
 do 
@@ -28,6 +29,11 @@ curl 'http://api.turinglabs.net/api/v1/jd/jxfactory/create/'${sharecode}'/' -H '
 done
 
 for sharecode in ${DDFACTORY_SHARECODES_ARR[@]}
+do 
+curl 'http://api.turinglabs.net/api/v1/jd/ddfactory/create/'${sharecode}'/' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.5,zh-HK;q=0.3,en;q=0.2' --compressed -H 'DNT: 1' -H 'Connection: keep-alive'
+done
+
+for sharecode in ${DREAM_FACTORY_SHARE_CODES_ARR[@]}
 do 
 curl 'http://api.turinglabs.net/api/v1/jd/ddfactory/create/'${sharecode}'/' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.5,zh-HK;q=0.3,en;q=0.2' --compressed -H 'DNT: 1' -H 'Connection: keep-alive'
 done
