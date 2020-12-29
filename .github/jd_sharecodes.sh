9b05e78e9b0df9656139f31170f38eb33853962e
zerokos <zerokos@qq.com>
2020-12-29 17:21:24 +0800

    11

--- .github/jd_sharecodes.sh
+++ .github/jd_sharecodes.sh
@@ -8,6 +8,7 @@ JXFACTORY_SHARECODES_ARR=($JXFACTORY_SHARECODES)
 DDFACTORY_SHARECODES_ARR=($DDFACTORY_SHARECODES)
 DREAM_FACTORY_SHARE_CODES_ARR=($DREAM_FACTORY_SHARE_CODES)
 JDZZ_INVITECODES_ARR=($JDZZ_INVITECODES)
+JDJOY_SHARECODES_ARR=($JDJOY_SHARECODES)
 
 for sharecode in ${FRUITSHARECODES_ARR[@]}
 do 
@@ -41,5 +42,10 @@ done
 
 for sharecode in ${JDZZ_INVITECODES_ARR[@]}
 do 
-curl 'http://code.chiang.fun/api/v1/jd/jdzz/create/'${sharecode}'/' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.5,zh-HK;q=0.3,en;q=0.2' --compressed -H 'DNT: 1' -H 'Connection: keep-alive'
+curl 'https://code.chiang.fun/api/v1/jd/jdzz/create/'${sharecode}'/' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.5,zh-HK;q=0.3,en;q=0.2' --compressed -H 'DNT: 1' -H 'Connection: keep-alive'
+done
+
+for sharecode in ${JDJOY_SHARECODES_ARR[@]}
+do 
+curl 'https://code.chiang.fun/api/v1/jd/jdcrazyjoy/create/'${sharecode}'/' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.5,zh-HK;q=0.3,en;q=0.2' --compressed -H 'DNT: 1' -H 'Connection: keep-alive'
 done
